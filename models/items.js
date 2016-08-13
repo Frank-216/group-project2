8d970357c2452bb7dd4f27dea170f2a6c9247825
Rangy Abines <rangyabines11@gmail.com>
2016-08-12 21:55:10 -0400

    addedfile for products

---
+++ models/items.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var ITEMS = sequelize.define('ITEMS', {
+    product: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    stock: DataTypes.INTEGER,
+    materials: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return ITEMS;
+};
