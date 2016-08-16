c298fe0f7842bf28a93e877859fd4805620f41a8
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-08-16 19:39:12 -0400

    Server routes updated

--- controllers/routes/htmlRoutes.js
+++ controllers/routes/htmlRoutes.js
@@ -1,30 +1,64 @@
-// var homeController = require('../home');
-// module.exports = function(app) {
-//   app.get('/', homeController.renderHome);
-// };
+// //Add Dependencies 
+// var express = require('express');
+// var router = express.Router();
+// var products = require('../models/items.js');
+// var Users = require('../models/Users.js');
+// var Images = require("..models/images.js");
 
-// module.exports = function(app) {
-//   app.get('/products', homeController.renderHome);
-// };
+var homeController = require('../home');
 
-// app.get('/', function(req, res) {
-//     res.render('index');
-// });
-// app.get('/about', function(req, res) {
-//     res.render('about');
-// });
-// app.get('/cart', function(req, res) {
-//     res.render('cart');
-// });
-// app.get('/contact', function(req, res) {
-//     res.render('contact');
-// });
-// app.get('/signin', function(req, res) {
-//     res.render('signin');
-// });
-// app.get('/products', function(req, res) {
-//     res.render('products');
-// });
-// app.get('/search', function(req, res) {
-//     res.render('search');
-// });
+module.exports = function(app) {
+  app.get('/', homeController.renderHome);
+ 
+
+
+	app.get('/', function(req, res) {
+	    res.render('index');
+	});
+	app.get('/about', function(req, res) {
+	    res.render('about');
+	});
+	app.get('/cart', function(req, res) {
+	    res.render('cart');
+	});
+	app.get('/contact', function(req, res) {
+	    res.render('contact');
+	});
+	app.get('/signin', function(req, res) {
+	    res.render('signin');
+	});
+	app.get('/products', function(req, res) {
+	    res.render('products');
+	});
+	app.get('/search', function(req, res) {
+	    res.render('search');
+	});
+
+	app.get('/products/:product', function(req, res) {
+     var product = req.params.product;
+     Items.findOne({
+           where: {
+              product: product
+           }
+     }).then(function(product) {
+          console.log('product', product);
+          res.render('product', {
+            product: product
+          });
+     });
+	});
+	app.get('/products/:product', function(req, res) {
+     var product = req.params.product;
+     Items.findOne({
+           where: {
+              product: product
+           }
+     }).then(function(product) {
+          console.log('product', product);
+          res.render('product', {
+            product: product
+          });
+     });
+
+});
+};
--- server.js
+++ server.js
@@ -4,28 +4,30 @@ var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var methodOverride = require('method-override');
 var app = express();
-var Images = require('./models')['Images'];
-Images.sync();
-require('dotenv').config();
+
+// require('dotenv').config();
 
 
 // require('dotenv').config();
 
-//Route config 
-var htmlRoutes = require('./controllers/routes/htmlRoutes');
-var apiRoutes = require('./controllers/routes/apiRoutes');
+
+
+// var apiRoutes = require('./controllers/routes/apiRoutes')(app);
 
 // db
 global.db = require('./models');
+//Route config 
 
-var Images = require('./models')['Images'];
-Images.sync({force:true});
-var Users = require('./models')['user'];
-Users.sync({force:true});
-var Items = require('./models')['ITEMS'];
-Items.sync({force:true});
+// var Images = require('./models')['Images'];
+// Images.sync();
+// var Users = require('./models')['user'];
+// Users.sync();
+// var Items = require('./models')['ITEMS'];
+// Items.sync();
 // set up preserver work 
 var app = express();
+
+
 //allows access to complete public domain
 app.use(express.static(__dirname + '/public'));
 
@@ -37,69 +39,75 @@ app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+// Set routes 
+var htmlRoutes =require('./controllers/routes/htmlRoutes')(app);
+// app.use("/",htmlRoutes);
 
-app.get('/', function(req, res) {
-    res.render('index');
-});
-app.get('/about', function(req, res) {
-    res.render('about');
-});
-app.get('/cart', function(req, res) {
-    res.render('cart');
-});
-app.get('/contact', function(req, res) {
-    res.render('contact');
-});
-app.get('/signin', function(req, res) {
-    res.render('signin');
-});
-app.get('/products', function(req, res) {
-    res.render('products');
-});
-app.get('/search', function(req, res) {
-    res.render('search');
-});
-app.get('/productInfo', function(req, res) {
-    res.render('productinfo');
-});
 
-//sets express engin for each product handlebars
-app.get('/products/:product', function(req, res) {
-     var product = req.params.product;
-     Items.findOne({
-           where: {
-              product: product
-           }
-     }).then(function(product) {
-          console.log('product', product);
-          res.render('product', {
-            product: product
-          });
-     });
+// // app.get('/', function(req, res) {
+// //     res.render('index');
+// // });
+// // app.get('/about', function(req, res) {
+// //     res.render('about');
+// // });
+// // app.get('/cart', function(req, res) {
+// //     res.render('cart');
+// // });
+// // app.get('/contact', function(req, res) {
+// //     res.render('contact');
+// // });
+// // app.get('/signin', function(req, res) {
+// //     res.render('signin');
+// // });
+// // app.get('/products', function(req, res) {
+// //     res.render('products');
+// // });
+// // app.get('/search', function(req, res) {
+// //     res.render('search');
+// // });
+// // app.get('/productInfo', function(req, res) {
+// //     res.render('productinfo');
+// // });
 
-});
-//set images routes fromt he image datebase
-/*app.get('/products/:product', function(req, res) {
-     var product = req.params.product;
-     Items.findOne({
-           where: {
-              product: product
-           }
-     }).then(function(product) {
-          console.log('product', product);
-          res.render('product', {
-            product: product
-          });
-     });
+// //sets express engin for each product handlebars
+// app.get('/products/:product', function(req, res) {
+//      var product = req.params.product;
+//      Items.findOne({
+//            where: {
+//               product: product
+//            }
+//      }).then(function(product) {
+//           console.log('product', product);
+//           res.render('product', {
+//             product: product
+//           });
+//      });
 
-});*/
+// });
+// //set images routes fromt he image datebase
+// app.get('/products/:product', function(req, res) {
+//      var product = req.params.product;
+//      Items.findOne({
+//            where: {
+//               product: product
+//            }
+//      }).then(function(product) {
+//           console.log('product', product);
+//           res.render('product', {
+//             product: product
+//           });
+//      });
+
+// });
 
 //set the port connection. Either heroku or local host 
 var port = process.env.PORT || 3000;
 
-
-// Launch server  
-app.listen(port, function() {
+db.sequelize.sync().then(function(){
+  app.listen(port, function() {
     console.log("Connected to " + port);
-})
+  })
+});
+// Launch server  
+
 
