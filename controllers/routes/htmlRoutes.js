65e40fde2d926c402846c48725fe48042312967a
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-08-18 20:00:04 -0400

    update

--- controllers/routes/htmlRoutes.js
+++ controllers/routes/htmlRoutes.js
@@ -2,29 +2,69 @@
 //COMMONG VARIABLES ON THE ROUTES PAGE
 var homeController = require('../home');
 var products = db.ITEMS;
-var users = db.users;
+var users = db.user;
 
 module.exports = function(app) {
   app.get('/', homeController.renderHome);
  
 
-
+  // render the index page 
 	app.get('/', function(req, res) {
 	    res.render('index');
 	});
+	// render the about page 
 	app.get('/about', function(req, res) {
 	    res.render('about');
 	});
+	//render the cart page 
 	app.get('/cart', function(req, res) {
 	    res.render('cart');
 	});
+	// render the contact page 
 	app.get('/contact', function(req, res) {
 	    res.render('contact');
 	});
+	// render the signin page. 
 	app.get('/signin', function(req, res) {
 	    res.render('signin');
 	});
 
+	
+	//create a login route to ensure that customers are able to sign in. 
+	app.get('/signin/',function (req,res){
+		var cred = req.body;
+		console.log(cred.username);
+
+		users.findOne({
+			where:{
+				user_name: cred.username
+			}
+		}).then(function(data){
+			// store user information in local storage
+			console.log(data);
+			var user = data; 
+
+		})
+	});
+	// register information 
+	app.post("/signin/",function(req,res){
+		console.log('post');
+		console.log(req.body);
+
+		users.create({
+			user_name: req.body.username,
+			email: req.body.email,
+			password: req.body.password,
+			street_name: req.body.address,
+	
+			phone_number: null
+
+			// zip:req.body.zipCode
+		}).then(function(data){
+			console.log(data);
+		})
+
+	})
 	// Display the products page using the find all function to read oru sequelize DB
 	app.get('/products', function(req, res) {
 			
@@ -56,5 +96,6 @@ module.exports = function(app) {
           });
      });
 	});
+
 	
 };
