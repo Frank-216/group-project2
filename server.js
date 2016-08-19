52c9e09b2c405822e2511e33c6f586c40dab2189
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-08-18 21:06:52 -0400

    removing stupid workd

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var methodOverride = require('method-override');
-var app = express();
-
 
 
 
