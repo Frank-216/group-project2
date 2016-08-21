7b8452b6536c8af61a0acad4ac78abfc7daf8496
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-08-21 14:31:37 -0400

    fixed errors

--- seeders/20160813144109-unnamed-seeder.js
+++ seeders/20160813144109-unnamed-seeder.js
@@ -23,21 +23,11 @@ module.exports = {
       {
         url  : 'https://wanderersessentials.herokuapp.com/img/bachelorSquare.jpg',
         createdAt: new Date(),
-<<<<<<< HEAD
-        updatedAt: new Date()
-      },{
-          product: 'The Wanderers Crate!',
-        price: 100.00,
-        stock:300,
-        materials:"6 Goats Milk Soap, Beard Balm, Wanders Shave Soap with Shave Brush, Coffee and Coconut Facial Scrub, Tobacco and Vanilla Soy Candle",
-        info:"Come and Wander into the Wanderers World of Essentials that you must have! With the Wanderers Crate, you are becoming an honorary Wanderer with a one stop shop of all the Essentials!",
-=======
         updatedAt: new Date(),
         ITEMId : 1
       },
       {
         url  : 'https://wanderersessentials.herokuapp.com/img/sandalwoodbourbonSquare.jpg',
->>>>>>> c944a0a8513b0939e3106fe71ea1eea486ff5e99
         createdAt: new Date(),
         updatedAt: new Date(),
         ITEMId : 6
